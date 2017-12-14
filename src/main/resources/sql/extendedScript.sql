c5a7fb58c206a6941ad5299e393a0ee5557c6b36
Siddharth Shah <shahsiddharth.10@gmail.com>
2017-12-13 23:36:06 -0600

    master - adding extended script for versions table

---
+++ src/main/resources/sql/extendedScript.sql
@@ -0,0 +1,6 @@
+ CREATE TABLE `versions` (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+   `version_id` bigint(20) DEFAULT NULL,
+   `v_number` bigint(20) DEFAULT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
